0a1fed64126308a946cad8d9907cb5ddc688d8cb
Dmitrii <zeionara@gmail.com>
2020-09-25 19:39:44 +0300

    Added more notifications on different stages of the queue lifecycle Added environment.yml, README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Google sheets notifier
+A simple app for making notifications via telegram when some special editions in a google sheets table occur.
+## Installing dependencies
+`conda env create --file environment.yml`
+## Launching the script
+`TG_API_KEY='XXX' CHAT_ID='XXX' SHEETS_ID='XXX' GOOGLE_SHEETS_API_CREDS_PATH='XXX' SHEET_ID='XXX' COMMAND_ID=0 python main.py`
--- main.py
+++ main.py
@@ -1,36 +1,53 @@
 import os
 import re
+from datetime import datetime
 from time import sleep
 from typing import List
 
-from GoogleSheetsAdapter import GoogleSheetsAdapter
-from TelegramAdapter import TelegramAdapter
+from adapters.GoogleSheetsAdapter import GoogleSheetsAdapter
+from adapters.TelegramAdapter import TelegramAdapter
 
+SECOND_NOTIFICATION_THRESHOLD = 3  # number of people before you
 COMMAND_NAME_PATTERN = re.compile(f'^[Бб].+{os.environ["COMMAND_ID"]}$')
-MESSAGE_PATTERN = f'Hey! {{name}} is waiting for you!\n{{link}}'
-DELAY = 10  # seconds
+MESSAGE_PATTERNS = {
+    'the-queue-has-begun': f'Hey! The queue has begun!',
+    'get-ready': f'Get ready! There are {SECOND_NOTIFICATION_THRESHOLD} people left before you in the queue',
+    'go': f'Hey! {{name}} is waiting for you!\n{{link}}'
+}
+DELAY = 5  # seconds
 
 
-def does_meet_conditions(row: List[str]):
-    return len(row) == 4 and row[0] == '' and COMMAND_NAME_PATTERN.fullmatch(row[1]) and len(row[2]) > 0 and len(row[3]) > 0
+def is_there_command(row: List[str]):
+    return len(row) >= 2 and row[0] == '' and COMMAND_NAME_PATTERN.fullmatch(row[1])
 
 
-def make_notification_message(rows: List[List[str]]):
-    for row in rows:
-        if does_meet_conditions(row):
-            return MESSAGE_PATTERN.format(name=row[2], link=row[3])
+def is_there_link(row: List[str]):
+    return len(row) >= 4 and len(row[2]) > 0 and len(row[3]) > 0
+
+
+def make_notification_message(rows: List[List[str]], n_notifications_: int):
+    for i, row in enumerate(rows):
+        if is_there_command(row) and not is_there_link(row) and n_notifications_ == 0:
+            return MESSAGE_PATTERNS['the-queue-has-begun']
+        elif is_there_command(row) and not is_there_link(row) and n_notifications_ == 1 and (
+                i >= SECOND_NOTIFICATION_THRESHOLD and is_there_link(rows[i - SECOND_NOTIFICATION_THRESHOLD - 1]) or i < SECOND_NOTIFICATION_THRESHOLD + 1
+        ):
+            return MESSAGE_PATTERNS['get-ready']
+        elif is_there_command(row) and is_there_link(row) and n_notifications_ == 2:
+            return MESSAGE_PATTERNS['go'].format(name=row[2], link=row[3])
 
 
 if __name__ == "__main__":
     telegram_adapter = TelegramAdapter(token=os.environ['TG_API_KEY'], chat_id=int(os.environ['CHAT_ID']))
     sheets_adapter = GoogleSheetsAdapter(spreadsheet_id=os.environ['SHEETS_ID'], api_key_path=os.environ['GOOGLE_SHEETS_API_CREDS_PATH'])
-    is_notified = False
-    while not is_notified:
-        notification_message = make_notification_message(
-            sheets_adapter.read(int(os.environ['SHEET_ID']))
-        )
+    n_notifications = 0
+    while True:
+        notification_message = make_notification_message(sheets_adapter.read(int(os.environ['SHEET_ID'])), n_notifications_=n_notifications)
         if notification_message is not None:
             telegram_adapter.send(notification_message)
-            is_notified = True
-        if not is_notified:
+            n_notifications += 1
+        if n_notifications < 3:
+            print(f'{datetime.now()} Hmm... There are no changes')
             sleep(DELAY)
+        else:
+            break
